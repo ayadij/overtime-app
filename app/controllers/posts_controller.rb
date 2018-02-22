4ff8864d69174a8872d786a0de968679c9c34a8a
ayadij <ayadijkwel@yahoo.com>
2018-02-22 10:49:08 -0700

    added delete button

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 
 class PostsController < ApplicationController
   
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   
   def index
     @posts = Post.all
@@ -12,7 +12,7 @@ class PostsController < ApplicationController
 
   def update
     if @post.update(post_params)
-      redirect_to @post, notice: 'Your post was created successfully'
+      redirect_to @post, notice: 'Your post was edited successfully'
     else
       render :edit
     end
@@ -33,6 +33,11 @@ class PostsController < ApplicationController
   end
 
   def show
+  end
+
+  def destroy
+    @post.delete
+    redirect_to posts_path, notice: 'Your post was deleted successfully'
   end
 
   private
