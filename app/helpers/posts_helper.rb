a7ff6799fb05a24a6851bb7e0701ee635d7c997b
ayadij <ayadijkwel@yahoo.com>
2018-03-02 14:01:58 -0700

    implemented labels with status values on index for posts with a custom view helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,18 @@
 module PostsHelper
+  def status_label status
+    status_span_generator status
+  end
+
+  private
+
+    def status_span_generator status
+      case status
+        when 'approved'
+          content_tag(:span, status, class: 'label label_success')
+        when 'submitted'
+          content_tag(:span, status, class: 'label label_primary')
+        when 'rejected'
+        content_tag(:span, status, class: 'label label_danger') 
+      end
+    end
 end
