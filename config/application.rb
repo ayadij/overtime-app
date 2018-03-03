303e6357f028dccb8dcbcbb21d2513f547ac5372
ayadij <ayadijkwel@yahoo.com>
2018-03-03 11:47:07 -0700

    implemented sms tool module in lib

--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Overtime
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    #config.active_record.raise_in_transactional_callbacks = true
+    config.autoload_paths << Rails.root.join("lib")
   end
 end
