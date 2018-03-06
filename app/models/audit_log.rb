81618d4a3491743230f92ee6709e1652fe15beb1
ayadij <ayadijkwel@yahoo.com>
2018-03-06 15:29:54 -0700

    integrated data validations with rspec tests. 33 examples, 0 failures

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -1,3 +1,12 @@
 class AuditLog < ApplicationRecord
+  validates_presence_of :user_id, :status, :start_date
   belongs_to :user
+  after_initialize :set_defaults
+
+
+  private
+
+    def set_defaults
+      self.start_date ||= Date.today - 6.days
+    end
 end
